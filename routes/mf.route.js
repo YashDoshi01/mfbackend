736b6b5b58b3bb1d73dc6e10a2402e5c0c6f51e7
Vedant Kesarwani <relativityeismc2@gmail.com>
2025-07-15 16:01:58 +0530

    Add mutual funds route and integrate NAV fetching
    - Created a new route for mutual funds with a greeting message.
    - Added a '/list-mf' endpoint to fetch and display NAVs using fetchAndPrintNAVs function.

--- routes/mf.route.js
+++ routes/mf.route.js
@@ -1,4 +1,11 @@
-import express from 'express';
-const mfRoute = express.Router();
+import express from 'express'
+import { fetchAndPrintNAVs } from '../index.js'
+const mfRoute = express.Router()
 
+mfRoute.get('/', (req, res) => res.send('Hello from Mutual Funds'))
 
+mfRoute.get('/list-mf', (req, res) => {
+    fetchAndPrintNAVs().then((result) => res.send(result))
+})
+
+export default mfRoute
